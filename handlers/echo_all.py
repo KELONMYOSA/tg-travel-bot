190bc948d5bbf4f9307783a19e10b869656741bb
Vladislav Samoilenko <79825223592@yandex.ru>
2023-09-11 23:52:02 +0300

    init commit

--- handlers/echo_all.py
+++ handlers/echo_all.py
@@ -1,8 +1,9 @@
 def run(bot):
     @bot.message_handler(func=lambda message: message.text not in ["/start", "/expo", "/party", "/standup"])
     async def echo_all(message):
+        message_text = message.text
         await bot.delete_message(message.chat.id, message.message_id)
         await bot.send_message(
             message.chat.id,
-            "Я пока таких команд не знаю"
+            "Я пока не знаю такую команду - \"" + message_text + "\""
         )
