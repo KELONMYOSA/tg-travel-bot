9c89821cf052a5344b3399078e07a422dfce4b25
Vladislav Samoilenko <79825223592@yandex.ru>
2023-09-12 15:53:58 +0300

    get events by domain name from db

--- utils/dao.py
+++ utils/dao.py
@@ -4,6 +4,8 @@ import psycopg2
 from psycopg2 import Error
 from dotenv import load_dotenv, find_dotenv
 
+from models.event import Event
+
 load_dotenv(find_dotenv())
 DATABASE_NAME = os.environ.get('DATABASE_NAME')
 DATABASE_USER = os.environ.get('DATABASE_USER')
@@ -22,14 +24,13 @@ def get_db_connection():
     )
 
 
-def db_query(query):
+def db_query(query: str):
     try:
         connection = get_db_connection()
 
         cursor = connection.cursor()
-        postgresql_query = query
 
-        cursor.execute(postgresql_query)
+        cursor.execute(query)
         result = cursor.fetchall()
 
         return result
@@ -40,3 +41,37 @@ def db_query(query):
         if connection:
             cursor.close()
             connection.close()
+
+
+def get_event_ids_by_domain_name(domain_name: str) -> list[int]:
+    domain_id = db_query(f"SELECT id FROM domain WHERE name = '{domain_name}'")[0][0]
+    event_ids = db_query(f"SELECT e_id FROM event_domain WHERE d_id = {domain_id}")
+    result = []
+    for event_id in event_ids:
+        result.extend(event_id)
+    return result
+
+
+def get_events_by_ids(event_ids: list[int]) -> list[Event]:
+    events_result = db_query(f"SELECT * FROM event WHERE id IN ({str(event_ids)[1:-1]})")
+    events = []
+    for event in events_result:
+        events.append(Event(
+            id=event[0],
+            m_id=event[1],
+            p_id=event[2],
+            original_text=event[3],
+            title=event[4],
+            long_desc=event[5],
+            short_desc=event[6],
+            date=event[7],
+            time_b=event[8],
+            time_e=event[9],
+            place=event[10],
+            geo_id=event[11],
+            url=event[12],
+            is_online=event[13],
+            date_add=event[14],
+            active=event[15]
+        ))
+    return events
